8caea185c87806d29a4acd602048b8efed32ad17
szymslow12 <szym.slow12@gmail.com>
2018-07-30 13:51:18 +0200

    add for test two game boards are added to <body>

--- app/src/main/resources/main.js
+++ app/src/main/resources/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
 import { GameBoard } from "./GameBoard.js";
-
-document.body.appendChild(new GameBoard().getGameBoard());
+let container = document.createElement("div");
+container.setAttribute("class", "game-boards-container");
+container.appendChild(new GameBoard().getGameBoard());
+container.appendChild(new GameBoard().getGameBoard());
+document.body.appendChild(container);
