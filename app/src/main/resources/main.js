874f637e484b80da062fd6db73ba1342ebf955e4
szymslow12 <szym.slow12@gmail.com>
2018-07-30 22:06:38 +0200

    add scaleGameBoardToBrowserZoomLevel/ click event to container for test

--- app/src/main/resources/main.js
+++ app/src/main/resources/main.js
@@ -3,6 +3,32 @@
 import { GameBoard } from "./GameBoard.js";
 let container = document.createElement("div");
 container.setAttribute("class", "game-boards-container");
-container.appendChild(new GameBoard().getGameBoard());
-container.appendChild(new GameBoard().getGameBoard());
+let gameBoards = [new GameBoard(), new GameBoard()]
+container.appendChild(gameBoards[0].getGameBoard());
+container.appendChild(gameBoards[1].getGameBoard());
+container.addEventListener("click", () => {
+    let squareContainers = document.getElementsByClassName("game-board");
+    console.log(`Container -> Width: ${container.offsetWidth} Height: ${container.offsetHeight}`);
+    for (let squareContainer of squareContainers) {
+        console.log(`GameBoard -> Width: ${squareContainer.offsetWidth} Height: ${squareContainer.offsetHeight}`);
+    }
+})
 document.body.appendChild(container);
+
+
+function scaleGameBoardToBrowserZoomLevel() {
+    let browserZoomLevel = Math.round(window.devicePixelRatio * 100);
+    window.setInterval(() => {
+    let multipler = browserZoomLevel / 100;
+    if (browserZoomLevel > 100) {
+        container.style.width = `${100 / multipler}%`;
+        container.style.height = `${100 / multipler}%`;
+    } else {
+        container.style.width = `${100}%`;
+        container.style.height = `${100}%`;
+    }
+    GameBoard.setSizeOfGameBoard(gameBoards);
+    });
+}
+
+scaleGameBoardToBrowserZoomLevel();
