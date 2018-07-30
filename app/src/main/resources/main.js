6f50fc1bfc54ab75a6e42d5502024971d18d6cd3
szymslow12 <szym.slow12@gmail.com>
2018-07-30 13:19:56 +0200

    remove unnecessary lines, <body> is now append by GameBoard

--- app/src/main/resources/main.js
+++ app/src/main/resources/main.js
@@ -1,12 +1,5 @@
 "use strict";
 
 import { GameBoard } from "./GameBoard.js";
-import {Square} from "./Square.js";
 
-let gameBoard = document.createElement("div");
-gameBoard.setAttribute("class", "game-board");
-let board = new GameBoard().getGameBoard();
-for (let square of board) {
-    gameBoard.appendChild(square.getHTMLSquare());
-}
-document.body.appendChild(gameBoard);
+document.body.appendChild(new GameBoard().getGameBoard());
