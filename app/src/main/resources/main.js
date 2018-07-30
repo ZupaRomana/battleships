91776abf031d993c052d1b01d13b15a03db62f2b
szymslow12 <szym.slow12@gmail.com>
2018-07-30 12:58:47 +0200

    add imports/board/for loop for tests

--- app/src/main/resources/main.js
+++ app/src/main/resources/main.js
@@ -1,6 +1,12 @@
 "use strict";
 
+import { GameBoard } from "./GameBoard.js";
+import {Square} from "./Square.js";
+
 let gameBoard = document.createElement("div");
 gameBoard.setAttribute("class", "game-board");
-gameBoard.textContent = "Battleships";
+let board = new GameBoard().getGameBoard();
+for (let square of board) {
+    gameBoard.appendChild(square.getHTMLSquare());
+}
 document.body.appendChild(gameBoard);
