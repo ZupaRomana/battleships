405bc16079d721ab4d4338080a2b1b94a5ea435f
Roman <adam.romanowski93@gmail.com>
2018-07-30 18:56:39 +0200

    Add classes responsible for loading static files

--- app/src/main/java/com/codecool/app/App.java
+++ app/src/main/java/com/codecool/app/App.java
@@ -1,13 +1,27 @@
 package com.codecool.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) {
+
+        HttpServer httpServer = null;
+
+        try {
+            httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        httpServer.createContext("/index", new LoginPage());
+        httpServer.createContext("/static", new Static());
+//        httpServer.createContext("/", new AccountController());
+
+        httpServer.setExecutor(null);
+
+        httpServer.start();
     }
 }
