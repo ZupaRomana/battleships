6b3ee0692a3ac918b4694ba7087bd974f71da37c
szymslow12 <szym.slow12@gmail.com>
2018-07-30 22:07:48 +0200

    add changeSizeOfSquare /change click event show pos and set color to red

--- app/src/main/resources/Square.js
+++ app/src/main/resources/Square.js
@@ -12,10 +12,25 @@ export class Square {
         this.div.setAttribute("class", "square");
         this.div.addEventListener("click", () => {
             alert(`x: ${this.xPos} y: ${this.yPos}`);
+            this.div.style.backgroundColor = "#ff3333"
+            console.log(`Width: ${this.div.offsetWidth} Height: ${this.div.offsetHeight}`);
         });
     }
 
     getHTMLSquare() {
         return this.div;
+    }
+
+    changeSizeOfSquare(boardWidth, boardHeight, browserZoomLevel) {
+        let multipler = browserZoomLevel / 100;
+        this.div.style.width = `${boardWidth * 0.09}px`;
+        this.div.style.height = `${boardHeight * 0.09}px`;
+        if (browserZoomLevel === 300) {
+            this.div.style.margin = "0px";
+        } else if (browserZoomLevel > 100) {  
+            this.div.style.margin = `${1 / multipler}px`;
+        } else {
+            this.div.style.margin = `${1}px`;
+        }
     }
 }
