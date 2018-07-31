7e0fca5d170ebd195e881e5f573e934d25c323a8
szymslow12 <szym.slow12@gmail.com>
2018-07-31 12:03:40 +0200

    remove addEventListener in loadAndFillContainer

--- app/src/main/resources/GameBoardsContainer.js
+++ app/src/main/resources/GameBoardsContainer.js
@@ -12,13 +12,6 @@ export class GameBoardsContainer {
         this.container = document.createElement("div");
         this.container.setAttribute("class", "game-boards-container");
         this.fillContaier();
-        this.container.addEventListener("click", () => {
-            let squareContainers = document.getElementsByClassName("game-board");
-            console.log(`Container -> Width: ${this.container.offsetWidth} Height: ${this.container.offsetHeight}`);
-            for (let squareContainer of squareContainers) {
-                console.log(`GameBoard -> Width: ${squareContainer.offsetWidth} Height: ${squareContainer.offsetHeight}`);
-            }
-        })
     }
 
     fillContaier() {
