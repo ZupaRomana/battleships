0cd5f12fce56f3e460d68f501049e08ab9f1de23
Mikolaj <mikolaj.rych@gmail.com>
2018-07-31 18:53:57 +0200

    implement reset map function

--- app/src/main/resources/tactics.js
+++ app/src/main/resources/tactics.js
@@ -1,31 +1,52 @@
 "use strict";
 import { Ship } from "./ship.js";
-
+import { GameBoard } from "./GameBoard.js";
 
 showTactics();
 function showTactics() {
-    let mainContainer = document.createElement("div");
-    document.body.appendChild(mainContainer);
+    let mainContainer = document.getElementById("main-container");
+    // document.body.appendChild(mainContainer);
 
     let resetButton = document.createElement("button");
     resetButton.setAttribute("id", "resetButton");
     resetButton.textContent = "RESET";
     mainContainer.appendChild(resetButton);
-    resetButton.addEventListener("click", function() {resetShips()});
 
-    // let map = createMap();
-    // mainContainer.appendChild(map);
+    let map = new GameBoard().getGameBoard();
+    mainContainer.appendChild(map);
+    resetButton.addEventListener("click", function () {
+        resetMap(mainContainer);
+        resetShips();
+    });
 
-    let shipsContainer = document.createElement("div");
-    shipsContainer.setAttribute("id", "shipsContainer");
-    mainContainer.appendChild(shipsContainer);
+    createShipsContainer(mainContainer);
 
 }
+function resetMap(mainContainer) {
+    mainContainer.removeChild(document.querySelector(".game-board"));
+    let map = new GameBoard().getGameBoard();
+    mainContainer.appendChild(map);
+    let container = document.querySelector("#shipsContainer");
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+    container.remove();
+    createShipsContainer(mainContainer);
+}
+
+function createShipsContainer(mainContainer) {
+    let shipsContainer = document.createElement("div");
+    shipsContainer.setAttribute("id", "shipsContainer");
+    shipsContainer.textContent = "Your ships";
+    mainContainer.appendChild(shipsContainer);
+}
+
 
 function resetShips() {
 
-    [1,1,1,1,2,2,2,3,3,4].forEach(element =>{
+    [1, 1, 1, 1, 2, 2, 2, 3, 3, 4].forEach(element => {
         let ship = new Ship(element);
-        ship.resetShip();});
+        ship.resetShip();
+    });
     document.getElementById("resetButton").setAttribute("disabled", true);
 }
