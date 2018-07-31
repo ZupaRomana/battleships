7fd2e01b79b72ae69c19857777ff49acb756c118
szymslow12 <szym.slow12@gmail.com>
2018-07-31 10:30:23 +0200

    change set to array / add getPlayerGameBoard/getEnemy.../getGameBoard

--- app/src/main/resources/GameBoard.js
+++ app/src/main/resources/GameBoard.js
@@ -4,23 +4,53 @@ import { Square } from "./Square.js";
 
 export class GameBoard {
     constructor() {
-        this.gameBoard = new Set();
+        this.gameBoard = [];
         this.loadSquares();
     }
 
     loadSquares() {
         for (let yPos = 0; yPos < 10; yPos++) {
             for (let xPos = 0; xPos < 10; xPos++) {
-                this.gameBoard.add(new Square(xPos, yPos));
+                let square = new Square();
+                square.xPos = xPos;
+                square.yPos = yPos;
+                this.gameBoard.push(square);
             }
         }
+    }
+
+    getPlayerGameBoard() {
+        return this.getGameBoard();
+    }
+
+    getEnemyGameBoard() {
+        let gameBoard = this.getGameBoard();
+        let enemySquares = gameBoard.childNodes;
+        for (let i = 0; i < enemySquares.length; i++) {//square of enemySquares) {
+            let square = this.gameBoard[i];
+            console.log(square);
+            let htmlSquare = enemySquares[i]; 
+            this.setEnemyGameBoardColorAddEvent(square, htmlSquare);
+            
+        }
+        return gameBoard;
+    }
+
+    setEnemyGameBoardColorAddEvent(square, htmlSquare) {
+        htmlSquare.style.backgroundColor = "cyan";
+        htmlSquare.addEventListener("click", () => {
+            alert(`x: ${square.xPos} y: ${square.yPos}`);
+            htmlSquare.style.backgroundColor = "#ff3333";
+            console.log(`Width: ${htmlSquare.offsetWidth} Height: ${htmlSquare.offsetHeight}`);
+        });
     }
 
     getGameBoard() {
         let gameBoard = document.createElement("div");
         gameBoard.setAttribute("class", "game-board");
         for (let square of this.gameBoard) {
-            gameBoard.appendChild(square.getHTMLSquare());
+            let htmlSquare = square.getHTMLSquare();
+            gameBoard.appendChild(htmlSquare);
         }
         return gameBoard;
     }
