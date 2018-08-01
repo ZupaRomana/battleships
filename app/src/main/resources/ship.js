ca5f2d4a2c042041eb371f2dd428c704f78675cc
Mikolaj <mikolaj.rych@gmail.com>
2018-08-01 17:29:08 +0200

    rewrite drag and drop functions, implement horizontal putting

--- app/src/main/resources/ship.js
+++ app/src/main/resources/ship.js
@@ -1,14 +1,22 @@
 "use strict";
 import { Square } from "./Square.js";
+import { GameBoard } from "./GameBoard.js";
 
+var ships = [];
+var _map;
+var id = 0;
 export class Ship {
 
-    constructor(size) {
+    constructor(size, map) {
         this.size = size;
         this.position = "horizontal";
-        this.drawShip();
-        this.width = 50;
-        this.height = size * 50;
+        this.width = size * 50;
+        this.height = 50;
+        _map = map;
+        this.id = id;
+        id++;
+        ships.push(this);
+        this.addDraggable();
     }
 
     drawShip() {
@@ -21,61 +29,100 @@ export class Ship {
         }
     }
 
+    addDraggable() {
+        document.addEventListener("dragstart", function (event) {
+            event.dataTransfer.setData("Text", event.target.id);
+        });
+        // document.addEventListener("dragenter", function(event) {
+        //     if ( event.target.className == "square" ) {
+        //         event.target.style.border = "3px dotted green";
+        //     }
+        // });
+
+        document.addEventListener("dragleave", function(event) {
+            if ( event.target.className == "square" ) {
+                event.target.style.border = "";
+            }
+        });
+
+        document.addEventListener("dragover", function(event) {
+            event.preventDefault();
+        });
+        document.addEventListener("drop", (event) => {
+            event.preventDefault();
+            if (event.target.className == "square") {
+                // event.target.style.backgroundColor = "black";
+                // event.target.setAttribute("isShip", true);
+                // event.target.setAttribute("droppable", false);
+                let data = event.dataTransfer.getData("Text");
+                let shipToRemove = document.getElementById(data);
+                document.getElementById("shipsContainer").removeChild(shipToRemove);
+
+                putShip(event, _map, ships[data - 1]);
+                $("#resetButton").attr("disabled", false);
+            }
+        })
+    }
+
     resetShip() {
+
         let container = document.getElementById("shipsContainer");
 
-        let div = document.createElement("div");
-        div.setAttribute("class", "shipDiv");
-        div.textContent = this.size;
-        container.appendChild(div);
+
 
         let ship = document.createElement("div");
-        ship.setAttribute("class", "free-ship");
+        ship.setAttribute("id", id);
         ship.setAttribute("class", `ship${this.size}`);
-        div.appendChild(ship);
+        container.appendChild(ship);
         ship.setAttribute("draggable", true);
+    }
+}
+    function putShip(event, map, ship) {
 
-        $(function () {
-            $(ship).draggable({
-                containment: '.game-board',
-                cursor: 'move',
-                revert: true
-                // stop: function(event) {
-                //     // $(event.target).remove();
+        let target = $(event.target.closest(".square"));
+        
+        let square = map.getSquare($(target).attr("xpos"), $(target).attr("ypos"));
+        square.isShip = true;
+        
+        colorAllSquares(square, ship);
+
+        // $(event.target.closest(".square")).css("backgroundColor", "black");
+        // $(event.target.closest(".square")).attr("isShip", true);
+        // square.updateDivColor();
+
+
+        // let ships = document.getElementsByClassName("shipDiv");
+        // for (let ship of ships) {
+        //     if (!ship.hasChildNodes()) {
+        //         ship.remove();
+        //     }
+        // }
+    }
+
+    function colorAllSquares(square, ship) {
+
+        // alert(ship.size);
+        if (ship.position === "vertical") {
+            for (let i = 0; i < ship.size; i++) {
+                let nextSquare = _map.getSquare(square.xPos, square.yPos + i);
+                nextSquare.isShip = true;
+                nextSquare.updateDivColor();
+            }
+
+        } else {
+            for (let i = 0; i < ship.size; i++) {
+                let nextSquare = _map.getSquare(square.xPos + i, square.yPos);
+                nextSquare.isShip = true;
+                nextSquare.getHTMLSquare().setAttribute("droppable", false);
+
+                // for (let i = 0; document.getElementById("#game-board").childNodes.length; i++) {
+                //     if (document.getElementById("#game-board").childNodes[i].getAttribute)
                 // }
-                });
-            });
-
-
-        $(function () {
-            $('.square').droppable({
-                drop: handleDrop
-            });
-        });
-
-        function checkSquare(event, ui) {
-            if (event.target.isShip) {
-                $(ui.draggable).css("backgroundColor", "red");
-            } else {
-                $(ui.draggable).css("backgroundColor", "green");
+                // $(".game-board").innerHTML = map.getPlayerGameBoard();
+                // $(".game-board").remove();
+                // document.getElementById("main-container").appendChild(_map.getPlayerGameBoard());
+                // document.getElementById("main-container").appendChild(ships);
+                nextSquare.updateDivColor();
             }
         }
-
-        function handleDrop(event, ui) {
-            ui.draggable.position({
-                of: $(this),
-                my: 'left top',
-                at: 'left top'
-            });
-            ui.draggable.draggable('option', 'revert', false);
-            ui.draggable.draggable("option", "grid", [49, 49]);
-            $(event.target).closest(".square").isShip = true;
-            // $(event.target).closest(".square").css("backgroundColor", "black");
-            $("#resetButton").attr("disabled", false);
-        }
-
-        ship.addEventListener("dblclick", function() {
-            this.position = "vertical";
-        });
-    }
 }
