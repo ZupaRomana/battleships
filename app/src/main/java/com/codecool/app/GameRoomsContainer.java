38ef639bafc97217f880334cdb89be0f8051dcd8
zuparomana <adam.romanowski93@gmail.com>
2018-08-02 18:57:43 +0200

    Implement GameRoomsContainer class

---
+++ app/src/main/java/com/codecool/app/GameRoomsContainer.java
@@ -0,0 +1,46 @@
+package com.codecool.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameRoomsContainer {
+    private static GameRoomsContainer instance;
+    private List<GameRoom> rooms;
+
+    private GameRoomsContainer() {
+        rooms = new ArrayList<GameRoom>();
+    }
+
+    public static GameRoomsContainer getInstance() {
+        if (instance == null) {
+            instance = new GameRoomsContainer();
+        }
+
+        return instance;
+    }
+
+    public void add(GameRoom gameRoom) {
+        rooms.add(gameRoom);
+    }
+
+    public GameRoom get(int index) {
+        return rooms.get(index);
+    }
+
+    public void remove(int index) {
+        rooms.remove(index);
+    }
+
+    public int getSize() { return rooms.size(); }
+
+    public List<GameRoom> getAll() { return rooms; }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (GameRoom room : rooms) {
+//            stringBuilder.append("#").append(room.getId()).append(" host Nickname: ").append(room.getHostPlayerName()).append(" SessionId: ").append(room.getHostingPlayerSessionId()).append(" client Nickname: ").append(room.getClientPlayerName()).append(" SessionId: ").append(room.getClientPlayerSessionId()).append("\n");
+            stringBuilder.append("#").append(room.getId()).append("#").append(room.getHostPlayerName()).append("#").append(room.getHostingPlayerSessionId()).append("#").append(room.getClientPlayerName()).append("#").append(room.getClientPlayerSessionId()).append("$");
+        }
+        return stringBuilder.toString();
+    }
+}
