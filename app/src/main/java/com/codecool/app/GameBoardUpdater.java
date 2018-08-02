f00ae0009a4e8dacd546be60cafa72956fbe6b3f
szymslow12 <szym.slow12@gmail.com>
2018-08-02 09:03:20 +0200

    refactor -> add getGameState/getBytesList/sendGameState

--- app/src/main/java/com/codecool/app/GameBoardUpdater.java
+++ app/src/main/java/com/codecool/app/GameBoardUpdater.java
@@ -8,49 +8,61 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameBoardUpdater implements HttpHandler {
 
-    private String response;
-    private int counterPost = 0;
-    private int counterGet = 0;
+    private String gameState;
 
     @Override
     public void handle(HttpExchange httpExchange) {
-        System.out.println("japierdole");
+
         String method = httpExchange.getRequestMethod();
         System.out.println(method);
         if (method.equals("POST")) {
-            InputStream is = httpExchange.getRequestBody();
-
-            int bytesNumber;
-            List<Byte> byteList = new ArrayList<>();
-            try {
-                while ((bytesNumber = is.read()) != -1) {
-                    byteList.add((byte) bytesNumber);
-                }
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            byte[] bytes = new byte[byteList.size()];
-            for (int i = 0; i < bytes.length; i++) {
-                bytes[i] = byteList.get(i);
-            }
-            response  = new String(bytes);
-            System.out.println( counterPost++ +" - "+response);
+            getGameState(httpExchange);
         } else if (method.equals("GET")) {
-            OutputStream os = httpExchange.getResponseBody();
-            try {
-                httpExchange.getResponseHeaders().set("Content-Type", "application/json");
-                httpExchange.sendResponseHeaders(200, response.length());
-                os.write(response.getBytes());
-                os.close();
-                System.out.println("\u001B[31m" + counterGet++ +" - "+new String(response.getBytes()) + "\u001B[0m");
-            } catch (IOException e) {
-                e.printStackTrace();
+            sendGameState(httpExchange);
+        }
+    }
+
+    private void getGameState(HttpExchange httpExchange) {
+        InputStream is = httpExchange.getRequestBody();
+
+        List<Byte> byteList = getBytesList(is);
+        byte[] bytes = new byte[byteList.size()];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = byteList.get(i);
+        }
+        gameState  = new String(bytes);
+        System.out.println(gameState);
+    }
+
+    private List<Byte> getBytesList(InputStream is) {
+        List<Byte> byteList = new ArrayList<>();
+        try {
+            int bytesNumber;
+            while ((bytesNumber = is.read()) != -1) {
+                byteList.add((byte) bytesNumber);
             }
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return byteList;
+    }
+
+    public void sendGameState(HttpExchange httpExchange) {
+        OutputStream os = httpExchange.getResponseBody();
+        try {
+            httpExchange.getResponseHeaders().set("Content-Type", "application/json");
+            httpExchange.sendResponseHeaders(200, gameState.length());
+            os.write(gameState.getBytes());
+            os.close();
+            System.out.println("\u001B[31m" + new String(gameState.getBytes()) + "\u001B[0m");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
