b0bb2e91ab2d6b94a64958a4f3b971d1d3db9fbc
Mikolaj <mikolaj.rych@gmail.com>
2018-08-02 13:58:56 +0200

    implement putting ship on map with all conditions

--- app/src/main/resources/ship.js
+++ app/src/main/resources/ship.js
@@ -17,37 +17,8 @@ export class Ship {
         ships.push(this);
     }
 
-    addDraggable() {
-        document.addEventListener("dragstart", function (event) {
-            event.dataTransfer.setData("Text", event.target.id);
-        });
-
-        document.addEventListener("dragleave", function (event) {
-            if (event.target.className == "square") {
-                event.target.style.border = "";
-            }
-        });
-
-        document.addEventListener("dragover", function (event) {
-            event.preventDefault();
-        });
-
-        document.addEventListener("drop", (event) => {
-            event.preventDefault();
-            if (event.target.className == "square") {
-                let data = event.dataTransfer.getData("Text");
-                let shipToRemove = document.getElementById(data);
-                document.getElementById("shipsContainer").removeChild(shipToRemove);
-
-                putShip(event, _map, ships[data - 1]);
-                $("#resetButton").attr("disabled", false);
-            }
-        })
-    }
-
     resetShip() {
 
-        this.addDraggable();
         let container = document.getElementById("shipsContainer");
 
         let ship = document.createElement("div");
@@ -67,15 +38,43 @@ export class Ship {
         ship.setAttribute("draggable", true);
     }
 }
-function putShip(event, map, ship) {
+
+addDraggable();
+
+function addDraggable() {
+    document.addEventListener("dragstart", function (event) {
+        event.dataTransfer.setData("Text", event.target.id);
+    });
+
+    document.addEventListener("dragleave", function (event) {
+        if (event.target.className == "square") {
+            event.target.style.border = "";
+        }
+    });
+
+    document.addEventListener("dragover", function (event) {
+        event.preventDefault();
+    });
+
+    document.addEventListener("drop", (event) => {
+        event.preventDefault();
+        if (event.target.className == "square" && event.target.getAttribute("droppable") === "true") {
+            let data = event.dataTransfer.getData("Text");
+            let target = $(event.target.closest(".square"));
+            putShip(target, _map, data);
+            $("#resetButton").attr("disabled", false);
+        };
+    });
+}
+
+
+function putShip(target, map, data) {
 
     counter++;
-    let target = $(event.target.closest(".square"));
 
     let square = map.getSquare($(target).attr("xpos"), $(target).attr("ypos"));
-    square.isShip = true;
 
-    colorAllSquares(square, ship);
+    colorAllSquares(square, data);
     checkShipsAmount();
 }
 
@@ -83,7 +82,7 @@ function checkShipsAmount() {
 
     if (counter == 10) {
         $("#saveButton").attr("disabled", false);
-       document.getElementById("saveButton").addEventListener("click", function() {saveToLocalStorage()});
+        document.getElementById("saveButton").addEventListener("click", function () { saveToLocalStorage() });
     }
 }
 
@@ -93,22 +92,96 @@ function saveToLocalStorage() {
     localStorage.setItem("map", allSquares);
 }
 
-function colorAllSquares(square, ship) {
+function colorAllSquares(square, data) {
+
+    let ship = ships[data - 1];
+    let squares = [];
 
     if (ship.position == "vertical") {
         for (let i = 0; i < ship.size; i++) {
             let nextSquare = _map.getSquare(square.xPos, square.yPos + i);
-            nextSquare.getHTMLSquare().style.backgroundColor = "black";
-            nextSquare.getHTMLSquare().setAttribute("droppable", false);
-            nextSquare.isShip = true;
+            squares.push(nextSquare);
         }
     } else {
         for (let i = 0; i < ship.size; i++) {
             let nextSquare = _map.getSquare(square.xPos + i, square.yPos);
-            nextSquare.isShip = true;
-            nextSquare.getHTMLSquare().style.backgroundColor = "black";
-            nextSquare.getHTMLSquare().setAttribute("isShip", true);
-            nextSquare.getHTMLSquare().setAttribute("droppable", false);
+            squares.push(nextSquare);
         }
+    };
+
+    checkAndChangeNextSquareStatus(squares, data);
+}
+
+function checkAndChangeNextSquareStatus(squares, data) {
+
+    for (let square of squares) {
+        if (typeof square === "undefined") {
+            squares = [];
+        }
+    }
+
+    if (squares.length > 0 && !isNeighbourhoodFree(squares)) {
+        for (let nextSquare of squares) {
+            nextSquare.getHTMLSquare().style.backgroundColor = "black";
+            nextSquare.getHTMLSquare().setAttribute("droppable", false);
+            nextSquare.getHTMLSquare().setAttribute("isShip", true);
+            nextSquare.isShip = true;
+        }
+
+        let shipToRemove = document.getElementById(data);
+        document.getElementById("shipsContainer").removeChild(shipToRemove);
+    }
+}
+
+function isNeighbourhoodFree(squares) {
+
+    for (let square of squares) {
+
+        let neighbours = getAllNeighbours(square);
+        
+        for (let neighbour of neighbours) {
+            if (typeof neighbour !== "undefined" && neighbour.isShip) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getAllNeighbours(square) {
+
+    let neighbours = [];
+    let rightNeighbour = getNeighbour(square, 1, 0);
+    neighbours.push(rightNeighbour);
+
+    let leftNeighbour = getNeighbour(square, -1, 0);
+    neighbours.push(leftNeighbour);
+
+    let topNeighbour = getNeighbour(square, 0, 1);
+    neighbours.push(topNeighbour);
+
+    let downNeighbour = getNeighbour(square, 0, -1);
+    neighbours.push(downNeighbour);
+
+    let rightTopNeighbour = getNeighbour(square, 1, 1);
+    neighbours.push(rightTopNeighbour);
+
+    let rightDownNeighbour = getNeighbour(square, 1, -1);
+    neighbours.push(rightDownNeighbour);
+
+    let leftTopNeighbour = getNeighbour(square, -1, 1);
+    neighbours.push(leftTopNeighbour);
+
+    let leftDownNeighbour = getNeighbour(square, -1, -1);
+    neighbours.push(leftDownNeighbour);
+
+    return neighbours;
+}
+
+function getNeighbour(square, xChange, yChange) {
+
+    let neighbour = _map.getSquare(square.xPos + xChange, square.yPos + yChange);
+    if (typeof neighbour !== "undefined") {
+        return neighbour;
     }
 }
