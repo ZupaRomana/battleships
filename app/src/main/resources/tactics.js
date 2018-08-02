8d7f32b414e9f04583b2d46b9726eb84caca0575
Mikolaj <mikolaj.rych@gmail.com>
2018-08-02 10:06:13 +0200

    add save button

--- app/src/main/resources/tactics.js
+++ app/src/main/resources/tactics.js
@@ -1,6 +1,7 @@
 "use strict";
 import { Ship } from "./ship.js";
 import { GameBoard } from "./GameBoard.js";
+// import { Lobby } from "./Lobby.js";
 
 showTactics();
 function showTactics() {
@@ -10,40 +11,67 @@ function showTactics() {
     resetButton.setAttribute("id", "resetButton");
     resetButton.textContent = "RESET";
 
-    let gameboard = new GameBoard();
-    let map = gameboard.getPlayerGameBoard();
-    mainContainer.appendChild(resetButton);
+    let saveButton = document.createElement("button");
+    saveButton.setAttribute("id", "saveButton");
+    saveButton.textContent = "SAVE";
 
+    let localData = localStorage.getItem("map");
+    let jsonArray = JSON.parse(localData);
+
+    let gameboard = new GameBoard();
+
+    let map = gameboard.getPlayerGameBoard(jsonArray);
+    
+    mainContainer.appendChild(resetButton);
+    mainContainer.appendChild(saveButton);
     mainContainer.appendChild(map);
+    
+    saveButton.addEventListener("click", function() {
+        // new Lobby().launch();
+    });
+
     resetButton.addEventListener("click", function () {
-        let newGameBoard = resetMap(gameboard, mainContainer);
+        $("#saveButton").attr("disabled", true);
+        localStorage.clear();
+        jsonArray = null;
+        let newGameBoard = resetMap(gameboard, mainContainer, jsonArray);
         resetShips(newGameBoard);
     });
 
     createShipsContainer(mainContainer);
 
 }
-function resetMap(gameboard, mainContainer) {
+function resetMap(gameboard, mainContainer, jsonArray) {
     
     mainContainer.removeChild(document.querySelector(".game-board"));
+
     let newGameBoard = new GameBoard();
-    let newMap = newGameBoard.getPlayerGameBoard();
+    let newMap = newGameBoard.getPlayerGameBoard(jsonArray);
+
     mainContainer.appendChild(newMap);
+
     let container = document.querySelector("#shipsContainer");
     container.remove();
+
     createShipsContainer(mainContainer);
+
     return newGameBoard;
 }
 
 function createShipsContainer(mainContainer) {
+
     let container = document.createElement("div");
     container.setAttribute("id", "myContainer");
+
     let shipsContainer = document.createElement("div");
     shipsContainer.setAttribute("id", "shipsContainer");
+
     let title = document.createElement("div");
     title.setAttribute("id", "header");
-    shipsContainer.appendChild(title);
     title.textContent = "Your ships";
+
+    shipsContainer.appendChild(title);
+    
     mainContainer.appendChild(shipsContainer);
 }
 
