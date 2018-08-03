c78083eb8d3b9f9db8cdca7d14add3695ffa337f
szymslow12 <szym.slow12@gmail.com>
2018-08-03 20:15:26 +0200

    add run()/checkIfPlayersAreInRoom()/getGameRoom()

--- app/src/main/resources/static/js/LobbyStatusChecker.js
+++ app/src/main/resources/static/js/LobbyStatusChecker.js
@@ -3,7 +3,28 @@
 export class LobbyStatusChecker {
     constructor() {
         this.httpExec = new XMLHttpRequest();
+        this.hasRoomPlayers = false;         
     }
-
     
+    run() {
+        this.checkIfPlayersAreInRoom();
+    }   
+
+    checkIfPlayersAreInRoom() {
+        getGameRoom(this.httpExec)
+        
+    }   
+}
+
+function getGameRoom(httpExec) {
+    httpExec.onreadystatechange = () => {
+        if (httpExec.status == 200 & httpExec.readyState == 4) {
+            localStorage.setItem("gameRoom", httpExec.responeText);
+            clearTimeout(getGameRoom)
+            console.log("Get game room!");
+        }
+    };
+    httpExec.open("GET", "/lobbyController", true);
+    httpExec.send(null);
+    var getRoomTimeout = setTimeout(() => { getGameRoom(httpExec); }, 1000)
 }
