00d6cd81d559855260717f221fd203fee8041e76
szymslow12 <szym.slow12@gmail.com>
2018-08-04 17:19:37 +0200

    add checkIfPlayersAreReady/checkIfGameRoom

--- app/src/main/resources/static/js/LobbyStatusChecker.js
+++ app/src/main/resources/static/js/LobbyStatusChecker.js
@@ -3,18 +3,30 @@
 export class LobbyStatusChecker {
     constructor() {
         this.httpExec = new XMLHttpRequest();
-        this.hasRoomPlayers = false;         
+        this.hasRoomPlayers = false; 
+        this.arePlayersReady = false;        
     }
     
     run() {
         this.checkIfPlayersAreInRoom();
+        this.checkIfPlayersAreReady();
     }   
 
     checkIfPlayersAreInRoom() {
-        console.log(this.httpExec);
-        getGameRoom(this.httpExec)
-        
-    }   
+        getGameRoom(this.httpExec);
+        checkIfGameRoom(this);   
+    }
+    
+    checkIfPlayersAreReady() {
+        if (this.gameRoom.hostIsReady & this.gameRoom.isPlayerReady) {
+            this.arePlayersReady = true;
+            console.log("Players are ready!");
+            clearTimeout(playersAreReadyTimeout);
+        }
+        var playersAreReadyTimeout = setTimeout(() => { 
+            this.checkIfPlayersAreReady();
+        }, 2000);
+    }
 }
 
 function getGameRoom(httpExec) {
@@ -24,7 +36,6 @@ function getGameRoom(httpExec) {
             if (json) {
                 localStorage.setItem("gameRoom", json);
                 clearTimeout(getRoomTimeout);
-                console.log("Get game room!");
             } else {
                 console.log("Bad game room download from server!")
             }
@@ -33,4 +44,18 @@ function getGameRoom(httpExec) {
     httpExec.open("GET", "/lobbyController", true);
     httpExec.send();
     var getRoomTimeout = setTimeout(() => { getGameRoom(httpExec); }, 2000);
+}
+
+function checkIfGameRoom(lobby) {
+    let gameRoom = localStorage.getItem("gameRoom"),;
+    if (gameRoom) {
+        lobby.hasRoomPlayers = true;
+        lobby.gameRoom = JSON.parse(gameRoom);
+        console.log("Get game room!");
+        clearTimeout(gameRoomTimeout);
+    }
+    var gameRoomTimeout = setTimeout(
+        () => { 
+            checkIfGameRoom(lobby);
+    }, 1000);
 }
