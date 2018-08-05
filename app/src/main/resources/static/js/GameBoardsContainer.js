3b70b7aeaa9f1f720a45b958ab099c7c07332aed
szymslow12 <szym.slow12@gmail.com>
2018-08-05 19:06:57 +0200

    change startGameBoard -> startGame/body is to change

--- app/src/main/resources/static/js/GameBoardsContainer.js
+++ app/src/main/resources/static/js/GameBoardsContainer.js
@@ -51,25 +51,9 @@ export class GameBoardsContainer {
     }
 }
 
-export let startGameBoard = function(gameBoardUpdater, redirectInterval) {
-
-        if (redirectInterval) {
-            clearInterval(redirectInterval);
-        }
-
-        let interval = setInterval(() => {
-            console.log("gameBoard interval");
-            gameBoardUpdater.getJSONFromServerInitial();
-         }, 2000);
-
-        if (localStorage.getItem("enemyMap")) {
-            clearInterval(interval);
-            gameBoardUpdater.postJSONToServer(localStorage.getItem("map"), true);
-            let gameBoardsContainer = new GameBoardsContainer();
-            document.getElementById("main-container").appendChild(gameBoardsContainer.container);
-            gameBoardsContainer.scaleGameBoardToBrowserZoomLevel();
-            document.getElementById("lobby").remove();
-        } else {
-            console.log("reasources not loaded");
-        }
+export function startGame() {    
+        let gameBoardsContainer = new GameBoardsContainer();
+        document.getElementById("main-container").appendChild(gameBoardsContainer.container);
+        gameBoardsContainer.scaleGameBoardToBrowserZoomLevel();
+        document.getElementById("lobby").remove();
     }
